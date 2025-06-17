71939f0c3d8151f80f5c635d1ef1a30393458652
Khangnghien <vuminhkhang2004@gmail.com>
2025-06-17 23:32:18 +0700

    Update Product.java
    Add field, constructor and get/set for product.

--- AE2CGroceryStore/src/main/java/model/Product.java
+++ AE2CGroceryStore/src/main/java/model/Product.java
@@ -10,4 +10,72 @@ package model;
  */
 public class Product {
     
+    private int productID;
+    private String productCode;
+    private String productName;
+    private int quantity;
+    private double price;
+    private Category categoryID;
+
+    public Product() {
+    }
+
+    public Product(int productID, String productCode, String productName, int quantity, double price, Category categoryID) {
+        this.productID = productID;
+        this.productCode = productCode;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+        this.categoryID = categoryID;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Category getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(Category categoryID) {
+        this.categoryID = categoryID;
+    }
+    
+    
 }
