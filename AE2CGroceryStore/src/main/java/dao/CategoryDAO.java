0c36adc3c9fb2cc3c036ccff142e0169830526bb
Khangnghien <vuminhkhang2004@gmail.com>
2025-06-18 10:38:20 +0700

    Create categoryDAO.java.
    Add categoryDAO to connect database and interact with it.

---
+++ AE2CGroceryStore/src/main/java/dao/CategoryDAO.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import DAO.ProductDAO;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Category;
+
+/**
+ *
+ * @author Vu Minh Khang - CE191371
+ */
+public class CategoryDAO extends dbconnect.DBContext {
+    
+    /**
+     * Get all the category from the database.
+     * 
+     * @return list of all category.
+     */
+    public List<Category> getAll() {
+        
+        List<Category> list = new ArrayList<>();
+        
+        try {
+            String query = "select c.CategoryID, c.CategoryName\n"
+                    + "from Category p \n";
+            PreparedStatement ps = this.getConnection().prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Category(rs.getInt(1),rs.getString(2)));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductDAO.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+        
+        return list;
+    }
+}
