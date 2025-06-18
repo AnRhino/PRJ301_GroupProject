93fb0b69b7866c2670c20f3cf7518d2537830bd5
AnNHPCE190747 <149403680+AnRhino@users.noreply.github.com>
2025-06-18 14:53:05 +0700

    fix bug
    change package of CategoryDAO.java

--- AE2CGroceryStore/src/main/java/controller/UserProductServlet.java
+++ AE2CGroceryStore/src/main/java/controller/UserProductServlet.java
@@ -5,7 +5,7 @@
 package controller;
 
 import DAO.ProductDAO;
-import dao.CategoryDAO;
+import DAO.CategoryDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
