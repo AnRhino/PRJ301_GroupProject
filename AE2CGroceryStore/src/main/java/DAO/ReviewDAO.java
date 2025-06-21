932a745e6cb5aa133ce1c16582bbe15441c03971
Khangnghien <vuminhkhang2004@gmail.com>
2025-06-21 23:19:34 +0700

    Update ReviewDAO.java
    Create a new method to get review by productID.

--- AE2CGroceryStore/src/main/java/DAO/ReviewDAO.java
+++ AE2CGroceryStore/src/main/java/DAO/ReviewDAO.java
@@ -41,4 +41,29 @@ public class ReviewDAO extends dbconnect.DBContext {
 
         return list;
     }
+    
+    public List<Review> getByProductID(int productID) {
+        List<Review> list = new ArrayList<>();
+        String query = "SELECT rv.ReviewID, us.UserID, prod.ProductID, rv.Rating , rv.Comment, YEAR(rv.ReviewDate) AS YearComment, MONTH(rv.ReviewDate) AS MonthComment, DAY(rv.ReviewDate) AS DayComment\n"
+                + "FROM [dbo].[Reviews] rv\n"
+                + "JOIN [dbo].[Products] prod\n"
+                + "ON prod.ProductID = rv.ProductID\n"
+                + "JOIN [dbo].[Users] us\n"
+                + "ON us.UserID = rv.UserID"
+                + "WHERE prod.ProductID = ?";
+        Object[] params = {productID};
+        
+        try {
+            ResultSet rs = execSelectQuery(query, params);
+
+            while (rs.next()) {
+                list.add(new Review(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getInt(4), rs.getString(5), new Date(rs.getInt(6), rs.getInt(7), rs.getInt(8))));
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ReviewDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return list;
+    }
 }
