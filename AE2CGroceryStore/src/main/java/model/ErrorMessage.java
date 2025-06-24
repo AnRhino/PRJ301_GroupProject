7c0170feec5097a85f5e6c03ba88e380335a14bb
AnNHPCE190747 <149403680+AnRhino@users.noreply.github.com>
2025-06-24 14:15:56 +0700

    Create ErrorMessage.java

---
+++ AE2CGroceryStore/src/main/java/model/ErrorMessage.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Nguyen Ho Phuoc An - CE190747
+ */
+public class ErrorMessage {
+
+    private String message;
+
+    public ErrorMessage() {
+    }
+
+    public ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
