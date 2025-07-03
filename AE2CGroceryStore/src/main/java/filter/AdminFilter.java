29558e9c5ea2ab129e6b32c846b5e3b4767975d2
Đinh Công Phúc <phuc.dinhcong813@gmail.com>
2025-07-03 21:42:13 +0700

    Create AdminFilter.java
    Prevents unauthorized access to admin-exclusive feature

---
+++ AE2CGroceryStore/src/main/java/filter/AdminFilter.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ *
+ * @author Dinh Cong Phuc - CE190770
+ */
+public class AdminFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest sr, ServletResponse sr1, FilterChain fc) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) sr;
+        HttpServletResponse res = (HttpServletResponse) sr1;
+        HttpSession session = req.getSession(false);
+
+        if (session == null) {
+            res.sendRedirect(req.getContextPath()); // Redirect to home
+        } else {
+            Integer roleId = (Integer) session.getAttribute("roleId");
+            if (roleId == null || roleId != 1) { // If not admin
+                res.sendRedirect(req.getContextPath()); // Redirect to home
+            } else { // If is admin
+                fc.doFilter(sr, sr1); // Admin access
+            }
+        }
+
+    }
+
+}
