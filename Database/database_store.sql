441ac5137edd213ac030ac61f29e86511bab95f1
AnNHPCE190747 <149403680+AnRhino@users.noreply.github.com>
2025-07-04 21:19:06 +0700

    Change database design
    Add ImagePath column for tables: Products, Categories, Users

--- Database/database_store.sql
+++ Database/database_store.sql
@@ -20,14 +20,16 @@ CREATE TABLE Users (
     Password NVARCHAR(100) NOT NULL,
     FullName NVARCHAR(100),
     Email NVARCHAR(100),
-    RoleID INT FOREIGN KEY REFERENCES Roles(RoleID)
+    RoleID INT FOREIGN KEY REFERENCES Roles(RoleID),
+	ImagePath VARCHAR(100)
 );
 
 -- Categories table
 CREATE TABLE Categories (
     CategoryID INT PRIMARY KEY IDENTITY(1,1),
     CategoryName NVARCHAR(100),
-	IsHidden BIT NOT NULL DEFAULT 0
+	IsHidden BIT NOT NULL DEFAULT 0,
+	ImagePath VARCHAR(100)
 );
 
 -- Products table
@@ -38,7 +40,8 @@ CREATE TABLE Products (
     Quantity INT NOT NULL,
     Price INT NOT NULL,
     CategoryID INT FOREIGN KEY REFERENCES Categories(CategoryID) NOT NULL,
-	IsHidden BIT NOT NULL DEFAULT 0
+	IsHidden BIT NOT NULL DEFAULT 0,
+	ImagePath VARCHAR(100)
 );
 
 -- Carts table
@@ -77,13 +80,14 @@ CREATE TABLE Orders (
     OrderID INT PRIMARY KEY IDENTITY(1,1),
     UserID INT FOREIGN KEY REFERENCES Users(UserID) NOT NULL,
     OrderDate DATE NOT NULL DEFAULT CAST(GETDATE() AS DATE),
+	DeliveryDate DATE,
     StatusID INT FOREIGN KEY REFERENCES StatusType(StatusID),
     DiscountCodeID INT FOREIGN KEY REFERENCES DiscountCodes(DiscountCodeID)
 );
 
 -- OrderDetails table
 CREATE TABLE OrderDetails (
-    OrderDetailID INT PRIMARY KEY IDENTITY,
+    OrderDetailID INT PRIMARY KEY IDENTITY(1,1),
     OrderID INT FOREIGN KEY REFERENCES Orders(OrderID),
     ProductID INT FOREIGN KEY REFERENCES Products(ProductID),
     Quantity INT,
@@ -92,12 +96,12 @@ CREATE TABLE OrderDetails (
 
 -- Reviews table
 CREATE TABLE Reviews (
-    ReviewID INT PRIMARY KEY IDENTITY,
+    ReviewID INT PRIMARY KEY IDENTITY(1,1),
     UserID INT FOREIGN KEY REFERENCES Users(UserID),
     ProductID INT FOREIGN KEY REFERENCES Products(ProductID),
     Rating INT CHECK (Rating BETWEEN 1 AND 5),
     Comment NVARCHAR(500),
-    ReviewDate DATE
+    ReviewTime DATETIME DEFAULT GETDATE()
 );
 
 -- Insert sample data for Roles table
