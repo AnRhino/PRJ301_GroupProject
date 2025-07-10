3af238f67b5228a7547f84305b4a5fc900c389a4
AnNHPCE190747 <149403680+AnRhino@users.noreply.github.com>
2025-07-10 20:55:59 +0700

    Update MessageConstants.java
    add constants

--- AE2CGroceryStore/src/main/java/utils/MessageConstants.java
+++ AE2CGroceryStore/src/main/java/utils/MessageConstants.java
@@ -15,9 +15,14 @@ public class MessageConstants {
     public final static String EMPTY_INPUT_MESSAGE = "Please enter something.";
     public final static String INVALID_INTEGER_INPUT_MESSAGE = "Please enter a valid integer number.";
     
+    // Error message for date type
+    public final static String INVALID_DATE = "An error occurred. Please select date again.";
+    
+    // Error message for Cart
     public final static String INVALID_CART_INPUT_MESSAGE = "The number is not available.";
     public final static String SUCCESS_CART_INPUT_MESSAGE = "Add to your cart successfully.";
     
+    // Error message for Comment
     public final static String EMPTY_COMMENT_INPUT_MESSAGE = "Please enter a comment for this product.";
     public final static String EMPTY_RATING_INPUT_MESSAGE = "Please select a rate for this product.";
     public final static String INVALID_RATING_INPUT_MESSAGE = "Please choose a valid rate for this product.";
@@ -28,5 +33,12 @@ public class MessageConstants {
     public final static String SUCCESS_EDIT_COMMENT_MESSAGE = "Successfully edit the comment.";
     public final static String ERROR_DELETE_COMMENT_MESSAGE = "Fail to delete the comment.";
     public final static String SUCCESS_DELETE_COMMENT_MESSAGE = "Successfully remove the comment.";
+    
+    //Error message for Order
+    public final static String DELIVERY_DATE_BEFORE_TODAY_MESSAGE = "Delivery date must not be before today";
+    public final static String ERROR_CREATE_NEW_ORDER = "Fail to create new order";
+    
+    //Error message for Discount Code
+    public final static String DISCOUND_CODE_NOT_FOUND = "Discount code is not found";
     
 }
