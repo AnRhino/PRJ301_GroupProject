745e95c98650bc3c67d5568a8f4d452a91502ff0
AnNHPCE190747 <149403680+AnRhino@users.noreply.github.com>
2025-07-11 14:43:59 +0700

    Update MessageConstants.java
    add new error message

--- AE2CGroceryStore/src/main/java/utils/MessageConstants.java
+++ AE2CGroceryStore/src/main/java/utils/MessageConstants.java
@@ -9,36 +9,39 @@ package utils;
  * @author Vu Minh Khang - CE191371
  */
 public class MessageConstants {
-    
+
     public final static String UNKNOWN_ERROR_MESSAGE = "There is an error.";
-    
+
     public final static String EMPTY_INPUT_MESSAGE = "Please enter something.";
     public final static String INVALID_INTEGER_INPUT_MESSAGE = "Please enter a valid integer number.";
-    
+
     // Error message for date type
     public final static String INVALID_DATE = "An error occurred. Please select date again.";
-    
+
     // Error message for Cart
     public final static String INVALID_CART_INPUT_MESSAGE = "The number is not available.";
     public final static String SUCCESS_CART_INPUT_MESSAGE = "Add to your cart successfully.";
-    
+
     // Error message for Comment
     public final static String EMPTY_COMMENT_INPUT_MESSAGE = "Please enter a comment for this product.";
     public final static String EMPTY_RATING_INPUT_MESSAGE = "Please select a rate for this product.";
     public final static String INVALID_RATING_INPUT_MESSAGE = "Please choose a valid rate for this product.";
     public final static String OUT_OF_RANGE_RATING_INPUT_MESSAGE = "Rating only from 1 to 5.";
-    
+
     public final static String UNKNOWN_COMMENT_MESSAGE = "There is an error with this comment. Please try again.";
     public final static String ERROR_EDIT_COMMENT_MESSAGE = "You must enter something in the comment.";
     public final static String SUCCESS_EDIT_COMMENT_MESSAGE = "Successfully edit the comment.";
     public final static String ERROR_DELETE_COMMENT_MESSAGE = "Fail to delete the comment.";
     public final static String SUCCESS_DELETE_COMMENT_MESSAGE = "Successfully remove the comment.";
-    
-    //Error message for Order
+
+    // Error message for Order
     public final static String DELIVERY_DATE_BEFORE_TODAY_MESSAGE = "Delivery date must not be before today";
     public final static String ERROR_CREATE_NEW_ORDER = "Fail to create new order";
-    
-    //Error message for Discount Code
+
+    // Error message for Order Item
+    public final static String ERROR_ADD_ITEM_INTO_ORDER = "Fail to add items into order";
+
+    // Error message for Discount Code
     public final static String DISCOUND_CODE_NOT_FOUND = "Discount code is not found";
-    
+
 }
