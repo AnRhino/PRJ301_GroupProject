a07a9d30b9df5a0e0e8cb39d9997b9dab196b012
AnNHPCE190747 <149403680+AnRhino@users.noreply.github.com>
2025-07-11 08:55:43 +0700

    Create Order.java

---
+++ AE2CGroceryStore/src/main/java/model/Order.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ *
+ * @author Nguyen Ho Phuoc An - CE190747
+ */
+public class Order {
+
+    private int id;
+    private User user;
+    private LocalDateTime orderDate;
+    private LocalDateTime deliveryDate;
+    private List<OrderItem> items;
+    private DiscountCode discount;
+    private OrderStatus status;
+    private String phoneNumber;
+    private String address;
+
+    public Order() {
+    }
+
+    public Order(int id, User user, LocalDateTime orderDate, LocalDateTime deliveryDate, List<OrderItem> items, DiscountCode discount, OrderStatus status, String phoneNumber, String address) {
+        this.id = id;
+        this.user = user;
+        this.orderDate = orderDate;
+        this.deliveryDate = deliveryDate;
+        this.items = items;
+        this.discount = discount;
+        this.status = status;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public LocalDateTime getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(LocalDateTime deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public DiscountCode getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(DiscountCode discount) {
+        this.discount = discount;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+}
