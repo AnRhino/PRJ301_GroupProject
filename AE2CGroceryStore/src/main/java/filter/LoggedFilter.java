4763fc1aaee50c045a10bc2cf275dcb5ac9a783e
AnNHPCE190747 <149403680+AnRhino@users.noreply.github.com>
2025-07-20 21:07:13 +0700

    add filter

---
+++ AE2CGroceryStore/src/main/java/filter/LoggedFilter.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ *
+ * @author Nguyen Ho Phuoc An - CE190747
+ */
+public class LoggedFilter implements Filter{
+
+    @Override
+    public void doFilter(ServletRequest sr, ServletResponse sr1, FilterChain fc) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) sr;
+        HttpServletResponse response = (HttpServletResponse) sr1;
+        HttpSession session = request.getSession();
+        
+        if(session != null && session.getAttribute("loggedUser") != null) {
+            response.sendRedirect(request.getContextPath());
+        } else {
+            fc.doFilter(sr, sr1);
+        }
+    }
+    
+}
